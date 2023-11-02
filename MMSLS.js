40712202cea10e7b7e443fced36e81c466dbdbaf
Space-FuCheng <73419850+Space-FuCheng@users.noreply.github.com>
2023-11-02 16:45:40 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -21,8 +21,6 @@ const MMSLS = [
 const MMSLS = [
     
     "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
-    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
-    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
   ]
   /**
    * 生成随机数字
