70bea3dd6faa4ce953bac07317969516106ab1c7
Space-FuCheng <73419850+Space-FuCheng@users.noreply.github.com>
2023-11-25 15:00:06 +0800

    Update USER_AGENTS.js

--- USER_AGENTS.js
+++ USER_AGENTS.js
@@ -102,33 +102,63 @@ const USER_AGENTS = [
   }
   // const USER_AGENT = USER_AGENTS[randomNumber(0, USER_AGENTS.length)];
 
-  const { exec } = require('child_process');
+//   const { exec } = require('child_process');
 
-  let USER_AGENT = ''; // 定义一个变量以存储 stdout
+//   let USER_AGENT = ''; // 定义一个变量以存储 stdout
   
-  exec('python ./fake-useragent-1.4.0/src/fake_useragent/fake.py', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行出错: ${error}`);
-      return;
-    }
-    USER_AGENT = stdout.trim();
+//   exec('python ./fake-useragent-1.4.0/src/fake_useragent/fake.py', (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`执行出错: ${error}`);
+//       return;
+//     }
+//     USER_AGENT = stdout.trim();
     
-  });
+//   });
   
 
-// const { exec } = require('child_process');
+// // const { exec } = require('child_process');
 
-// exec('python C:/Users/FuCheng/Desktop/test.py', (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`执行出错: ${error}`);
-//     return;
+// // exec('python C:/Users/FuCheng/Desktop/test.py', (error, stdout, stderr) => {
+// //   if (error) {
+// //     console.error(`执行出错: ${error}`);
+// //     return;
+// //   }
+// //   const USER_AGENT = stdout.trim();
+// //   console.log(`脚本输出: ${stdout}`);
+// // });
+
+// module.exports = {
+//     USER_AGENT
 //   }
-//   const USER_AGENT = stdout.trim();
-//   console.log(`脚本输出: ${stdout}`);
-// });
 
-module.exports = {
-    USER_AGENT
+const { exec } = require('child_process');
+
+function getUserAgent() {
+  return new Promise((resolve, reject) => {
+    exec('python ./fake-useragent-1.4.0/src/fake_useragent/fake.py', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行出错: ${error}`);
+        reject(error);
+        return;
+      }
+      const USER_AGENT = stdout.trim();
+      console.log(`脚本输出: ${USER_AGENT}`); // 在回调函数中输出 stdout
+      resolve(USER_AGENT); // 将值 resolve 出去
+    });
+  });
+}
+
+// 在异步函数中调用获取 USER_AGENT 的函数
+async function runScript() {
+  try {
+    const USER_AGENT = await getUserAgent(); // 等待获取 USER_AGENT 的值
+    console.log('获取到的 USER_AGENT:', USER_AGENT);
+    module.exports = { USER_AGENT }; // 导出 USER_AGENT 变量
+  } catch (error) {
+    console.error('获取 USER_AGENT 失败:', error);
   }
+}
+
+runScript(); // 执行异步函数
 
   
