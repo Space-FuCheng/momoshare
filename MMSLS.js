b18284449f537ff2988327e305598a7fa61892c4
Space-FuCheng <73419850+Space-FuCheng@users.noreply.github.com>
2024-01-05 09:24:20 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=32454734&pid=2e303d2ef71cdd434b393f5a680a857b&tid=64b0afc6c9cbf1b164e6b64a2f399615",
+    "https://www.maimemo.com/share/page?uid=32454734&pid=166de179b098286f6b7b274a9cc30498&tid=b34438f752d65ba7263327b71be3fdda",
 
 
   ]
